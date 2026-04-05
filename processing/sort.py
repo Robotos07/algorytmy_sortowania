974c5cd4b4dca1fb5f03eec5b6161b8a429d506c
Robotos <kuba_jakub2005.99@wp.pl>
2026-04-05 13:30:26 +0200

    Implementacja selection sort.

--- processing/sort.py
+++ processing/sort.py
@@ -10,12 +10,23 @@ def insertion_sort(unsorted_array):
         unsorted_array[i + 1] = key;
     return unsorted_array;
 
+def selection_sort(unsorted_array):
+    for idx in range(len(unsorted_array) - 1):
+        min_idx = idx
+
+        for j in range(idx + 1, len(unsorted_array)):
+            if unsorted_array[j] < unsorted_array[min_idx]:
+                min_idx = j
+
+        unsorted_array[idx], unsorted_array[min_idx] = unsorted_array[min_idx], unsorted_array[idx]
+    return unsorted_array;
+
 def main():
-    print(insertion_sort([6, 3, 1, 6, 8, 7]))
-    print(insertion_sort([x*x for x in range(10)]))
+    print(selection_sort([6, 3, 1, 6, 8, 7]))
+    print(selection_sort([x*x for x in range(10)]))
     rand_arr = [random.randint(1, 100) for x in range(200)]
     print(rand_arr);
-    print(insertion_sort(rand_arr))
+    print(selection_sort(rand_arr))
 
     return 0;
 
