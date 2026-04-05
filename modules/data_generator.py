d73e92ca92eb305f8ec70ae36193112bff329b56
Robotos <kuba_jakub2005.99@wp.pl>
2026-04-05 17:23:52 +0200

    Implementacja zestawu danych stałych

--- modules/data_generator.py
+++ modules/data_generator.py
@@ -13,6 +13,9 @@ def generate_ascending_array(n=10, min=1, max=10):
 def generate_descending_array(n=10, min=1, max=10):
     return generate_ascending_array(n, min, max)[::-1];
 
+def generate_flat_array(n=10):
+    return [1 for _ in range(n)];
+
 def main():
 
     #print(generate_random_array(1))
@@ -21,8 +24,10 @@ def main():
     #print(generate_random_array(min=1, max=10))
     #print(generate_random_array(min=10, max=1000))
 
-    print(generate_ascending_array());
-    print(generate_descending_array());
+    #print(generate_ascending_array());
+    #print(generate_descending_array());
+
+    print(generate_flat_array());
 
     return 0;
 
