66a9f0f76e9cea30d9eb1a74a61746dbad5183cf
Robotos <kuba_jakub2005.99@wp.pl>
2026-04-05 13:34:15 +0200

    Implementacja heap sort.

--- processing/sort.py
+++ processing/sort.py
@@ -21,12 +21,38 @@ def selection_sort(unsorted_array):
         unsorted_array[idx], unsorted_array[min_idx] = unsorted_array[min_idx], unsorted_array[idx]
     return unsorted_array;
 
+def heapify(unsorted_array, n, i):
+    largest = i    
+    l = 2 * i + 1    
+    r = 2 * i + 2  
+
+    if l < n and unsorted_array[l] > unsorted_array[largest]:
+        largest = l
+
+    if r < n and unsorted_array[r] > unsorted_array[largest]:
+        largest = r
+
+    if largest != i:
+        unsorted_array[i], unsorted_array[largest] = unsorted_array[largest], unsorted_array[i]
+        heapify(unsorted_array, n, largest)
+
+def heap_sort(unsorted_array):
+    n = len(unsorted_array)
+
+    for i in range(n // 2 - 1, -1, -1): # Build Heap
+        heapify(unsorted_array, n, i)
+
+    for i in range(n - 1, 0, -1):
+        unsorted_array[i], unsorted_array[0] = unsorted_array[0], unsorted_array[i]  # Swap max to end
+        heapify(unsorted_array, i, 0)
+    return unsorted_array;
+
 def main():
-    print(selection_sort([6, 3, 1, 6, 8, 7]))
-    print(selection_sort([x*x for x in range(10)]))
+    print(heap_sort([6, 3, 1, 6, 8, 7]))
+    print(heap_sort([x*x for x in range(10)]))
     rand_arr = [random.randint(1, 100) for x in range(200)]
     print(rand_arr);
-    print(selection_sort(rand_arr))
+    print(heap_sort(rand_arr))
 
     return 0;
 
