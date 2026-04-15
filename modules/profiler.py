79b05103e7f18e3190974e7cd607e74393673abd
mati183 <mateuszcieslak54@gmail.com>
2026-04-15 12:40:25 +0200

    edycja kodów i dodanie pliku z wykresami

--- modules/profiler.py
+++ modules/profiler.py
@@ -30,10 +30,230 @@ def profile_multiple(n_points=[100], func='print', repeats=1, setup=False, modul
     save_profile(message, filename)
     return 0;
 
+#IS RANDOM
 def main():    
     Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
     #profile(func='test_insertion_sort_random', module='tests', start=1000, end=1001, setup=True, step=1);
-    profile_multiple(func='test_insertion_sort_random', module='tests', n_points=[1000, 5000, 10000], repeats=1, setup=True);
+    profile_multiple(func='test_insertion_sort_random', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#IS D
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_insertion_sort_descending', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#IS A
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_insertion_sort_ascending', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#IS F
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_insertion_sort_flat', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#IS V
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_insertion_sort_v_shape', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#SS RANDOM
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_selection_sort_random', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#SS D
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_selection_sort_descending', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#SS A
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_selection_sort_ascending', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#SS F
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_selection_sort_flat', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#SS V
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_selection_sort_v_shape', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#HS RANDOM
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_heap_sort_random', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#HS D
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_heap_sort_descending', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#HS A
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_heap_sort_ascending', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#HS F
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_heap_sort_flat', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#HS V
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_heap_sort_v_shape', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#MS RANDOM
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_merge_sort_random', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#MS D
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_merge_sort_descending', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#MS A
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_merge_sort_ascending', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#MS F
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_merge_sort_flat', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#MS V
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_merge_sort_v_shape', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#QS R
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_quick_sort_random', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+#QS D
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_quick_sort_descending', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+
+#QS A
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_quick_sort_ascending', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+
+#QS F
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_quick_sort_flat', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
+    
+    Progress.advance_progress();
+
+    return 0;
+
+
+#QS V
+def main():    
+    Progress = progress_bar.Progress(tasks=1, timestamps=[0, 100], summary=True);
+    profile_multiple(func='test_quick_sort_v_shape', module='tests', n_points=[1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000], repeats=10, setup=True);
     
     Progress.advance_progress();
 
--- modules/tests.py
+++ modules/tests.py
@@ -24,6 +24,259 @@ def test_insertion_sort_ascending(x, save_raw=False, save_result=False, filename
 
     return 0;
 
+def test_insertion_sort_descending(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_descending_array(x);
+    result = sort.insertion_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_insertion_sort_flat(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_flat_array(x);
+    result = sort.insertion_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_insertion_sort_v_shape(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_v_shape_array(x);
+    result = sort.insertion_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_selection_sort_random(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_random_array(x);
+    result = sort.selection_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_selection_sort_ascending(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_ascending_array(x);
+    result = sort.selection_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_selection_sort_descending(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_descending_array(x);
+    result = sort.selection_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_selection_sort_flat(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_flat_array(x);
+    result = sort.selection_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_selection_sort_v_shape(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_v_shape_array(x);
+    result = sort.selection_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_heap_sort_random(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_random_array(x);
+    result = sort.heap_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_heap_sort_ascending(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_ascending_array(x);
+    result = sort.heap_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_heap_sort_descending(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_descending_array(x);
+    result = sort.heap_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_heap_sort_flat(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_flat_array(x);
+    result = sort.heap_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_heap_sort_v_shape(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_v_shape_array(x);
+    result = sort.heap_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_merge_sort_random(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_random_array(x);
+    result = sort.merge_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_merge_sort_ascending(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_ascending_array(x);
+    result = sort.merge_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_merge_sort_descending(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_descending_array(x);
+    result = sort.merge_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_merge_sort_flat(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_flat_array(x);
+    result = sort.merge_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_merge_sort_v_shape(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_v_shape_array(x);
+    result = sort.merge_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_quick_sort_random(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_random_array(x);
+    result = sort.quick_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_quick_sort_ascending(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_ascending_array(x);
+    result = sort.quick_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_quick_sort_descending(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_descending_array(x);
+    result = sort.quick_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_quick_sort_flat(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_flat_array(x);
+    result = sort.quick_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
+def test_quick_sort_v_shape(x, save_raw=False, save_result=False, filename_raw='data_raw_output', filename_result='data_result_output'):
+    raw = data_generator.generate_v_shape_array(x);
+    result = sort.quick_sort(raw[:]);
+
+    if save_raw:
+        file.save_data(raw, filename_raw);
+    if save_result:
+        file.save_data(result, filename_result);
+
+    return 0;
+
 def main():
     return 0;
 
