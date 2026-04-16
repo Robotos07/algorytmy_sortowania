4fba85aee65afe182a136ca76b4f0dfadce336c9
Robotos <kuba_jakub2005.99@wp.pl>
2026-04-16 22:51:24 +0200

    Zmiana implementacji generowania zakresu rosnącego, żeby uniknąć błędu z zbyt dużą głębią rekurencji.

--- modules/data_generator.py
+++ modules/data_generator.py
@@ -4,11 +4,14 @@ def generate_random_array(n=10, min=1, max=100):
     return [randint(min, max) for _ in range(n)];
 
 def generate_ascending_array(n=10, min=1, max=10):
-    if n == 1: 
-        return [randint(min, max)];
+    array = [];
+    current = randint(min, max);
 
-    arr = generate_ascending_array(n-1, min, max);
-    return arr + [arr[len(arr) - 1] + randint(min, max)];
+    for _ in range(n):
+        current += randint(min, max);
+        array.append(current);
+
+    return array;
 
 def generate_descending_array(n=10, min=1, max=10):
     return generate_ascending_array(n, min, max)[::-1];
@@ -18,7 +21,7 @@ def generate_flat_array(n=10):
 
 def generate_v_shape_array(n=10, min=1, max=10):
     if n % 2 == 0:
-        arr = generate_ascending_array(n / 2, min, max);
+        arr = generate_ascending_array(n // 2, min, max);
         return arr[::-1] + arr;
     else:
         arr = generate_ascending_array((n + 1) // 2, min, max);
@@ -32,8 +35,8 @@ def main():
     #print(generate_random_array(min=1, max=10))
     #print(generate_random_array(min=10, max=1000))
 
-    #print(generate_ascending_array());
-    #print(generate_descending_array());
+    print(generate_ascending_array());
+    print(generate_descending_array());
 
     #print(generate_flat_array());
 
